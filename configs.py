b00645b7cf25b96b6e6e027649c30995bf0ba974
private388 <131282633+private388@users.noreply.github.com>
2023-05-15 00:09:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,9 +1,9 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", ""))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "21114403"))
+    API_HASH = getenv("API_HASH", "192c2b697e9f45650354fd47292ccf78")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5973598173:AAG-WBeBVZuHg9t_HGE9rHfcE84DfsC0Ues")
     FSUB = getenv("FSUB", "SDBotz")
     CHID = int(getenv("CHID", "-1001876567760"))
     SUDO = list(map(int, getenv("SUDO").split()))
