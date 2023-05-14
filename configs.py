6015213d614cad080d4a805e937e0e2baa7bc221
private388 <131282633+private388@users.noreply.github.com>
2023-05-14 23:42:26 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,8 +5,8 @@ class Config:
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", ""))
+    CHID = int(getenv("CHID", "-1001876567760"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://nejot19048:R3gJAQO3BzgEorUp@cluster0.twqlcdy.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
